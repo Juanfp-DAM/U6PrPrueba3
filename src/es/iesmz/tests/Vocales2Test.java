d9e3a199087e0d0541b875a53acd2ccaee2129f7
Juanfp <juaferpui@alu.edu.gva.es>
2024-04-04 19:05:45 +0200

    Se crea el cuarto y último test

--- src/es/iesmz/tests/Vocales2Test.java
+++ src/es/iesmz/tests/Vocales2Test.java
@@ -33,4 +33,24 @@ class Vocales2Test {
         //ASSERT
         assertEquals("Jv ml Mglln", molaSinV);
     }
+
+    @org.junit.jupiter.api.Test
+    void sinVocales4(){
+        //ARRANGE
+        String numeros="4h0r4 s0n num3r0s";
+        //ACT
+        String numerosSinV=Vocales2.sinVocales(numeros);
+        //ASSERT
+        assertEquals("4h0r4 s0n nm3r0s", numerosSinV);
+    }
+
+    @org.junit.jupiter.api.Test
+    void sinVocales5(){
+        //ARRANGE
+        String cad="Cadena Sin Vocales";
+        //ACT
+        String cadSinV=Vocales2.sinVocales(cad);
+        //ASSERT
+        assertEquals("Cdn Sn Vcls", cadSinV);
+    }
 }
