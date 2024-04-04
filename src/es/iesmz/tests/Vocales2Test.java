8d65ca2e7caee3abf46aa85ff0902187ac1760c3
Juanfp <juaferpui@alu.edu.gva.es>
2024-04-04 19:00:26 +0200

    Se crea el segundo test y tercer test

--- src/es/iesmz/tests/Vocales2Test.java
+++ src/es/iesmz/tests/Vocales2Test.java
@@ -14,5 +14,23 @@ class Vocales2Test {
         assertEquals("Hl Mnd", holaSinVocales);
     }
 
-    
+    @org.junit.jupiter.api.Test
+    void sinVocales2(){
+        //ARRANGE
+        String cadena ="Esto Es una cadenA";
+        //ACT
+        String cadenaSinV=Vocales2.sinVocales(cadena);
+        //ASSERT
+        assertEquals("st s n cdn", cadenaSinV);
+    }
+
+    @org.junit.jupiter.api.Test
+    void sinVocales3(){
+        //ARRANGE
+        String mola = "Java mola MogollOn";
+        //ACT
+        String molaSinV=Vocales2.sinVocales(mola);
+        //ASSERT
+        assertEquals("Jv ml Mglln", molaSinV);
+    }
 }
